1e2c0c6ed67ee802b2c1aca99f25366229b4cf38
Piotr Mościcki <m.piotr@moscicki.pl>
2021-11-25 20:55:36 +0000

    Add more virtual methods to type related to construction and destruction.

--- src/rtti/type.hpp
+++ src/rtti/type.hpp
@@ -29,22 +29,29 @@ namespace rtti {
         const Attributes& attributes() const { return m_attributes; }
         template <typename ATTRIBUTE> const ATTRIBUTE* attribute() const;
 
+        bool is_copyable() const = 0;
+        bool is_movable() const = 0;
+        bool is_fundamental() const = 0;
+        bool is_enum() const = 0;
+        bool is_class() const = 0;
+
         virtual Object new_object() const = 0;
         virtual Object new_copy(const ObjectRef& src) const = 0;
         virtual Object new_move(ObjectRef& src) const = 0;
         virtual void delete_object(Object&& obj) const = 0;
 
-        bool is_copyable() const;
-        bool is_movable() const;
-
         virtual Res<void, ErrConstruct> can_construct(const BufferRef& buff) const = 0;
         virtual ObjectRef construct(BufferRef&& buff) const = 0;
         virtual Object construct(Buffer&& buff) const = 0;
+
         virtual bool can_copy_construct(const BufferRef& buff, const ObjectRef& src) const = 0;
         virtual ObjectRef copy_construct(BufferRef&& buff, const ObjectRef& src) const = 0;
         virtual Object copy_construct(Buffer&& buff, const ObjectRef& src) const = 0;
+        
+        virtual bool can_move_construct(const BufferRef& buff, const ObjectRef& src) const = 0;
         virtual ObjectRef move_construct(BufferRef&& buff, ObjectRef& src) const = 0;
         virtual Object move_construct(Buffer&& buff, ObjectRef& src) const = 0;
+        
         virtual BufferRef destruct(ObjectRef&& obj) const = 0;
         virtual Buffer destruct(Object&& obj) const = 0;
         
