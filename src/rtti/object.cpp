5451bf27b0e56c680c36c8c240584eef4deb4983
Piotr Mościcki <m.piotr@moscicki.pl>
2021-11-25 07:14:02 +0000

    WIP adding `Res to Object and Buffer types

--- src/rtti/object.cpp
+++ src/rtti/object.cpp
@@ -14,17 +14,29 @@ ObjectRef::ObjectRef(void* obj, const Type* type)
 
 //*************************************************************************************************
 Res<void*, ErrValue> ObjectRef::value() {
-
+    if (is_valid())
+        return Ok(m_value);
+    else
+        return Err(ErrValue::INVALID_OBJECT);
 }
 
 //*************************************************************************************************
 Res<const void*, ErrValue> ObjectRef::value() const {
-
+    if (is_valid())
+        return Ok(const_cast<const void*>(m_value));
+    else
+        return Err(ErrValue::INVALID_OBJECT);
 }
 
 //*************************************************************************************************
 Res<void*, ErrValue> ObjectRef::steal_value() && {
+    if (!is_valid())
+        return Err(ErrValue::INVALID_OBJECT);
 
+    void* res = m_value;
+    m_value = nullptr;
+    m_type = nullptr;
+    return Ok(res);
 }
 
 //*************************************************************************************************
--- src/rtti/type.hpp
+++ src/rtti/type.hpp
@@ -13,6 +13,15 @@ namespace rtti {
     //*********************************************************************************************
     class S4_RTTI_EXPORT Type {
     public:
+        enum class ErrConstruct {
+            INVALID_BUFFER
+        };
+
+        enum class ErrCopyConstruct {
+            INVALID_BUFFER,
+
+        }
+
         Type(std::string name, size_t size, Attributes attributes);
 
         const std::string& name() const { return m_name; }
@@ -21,13 +30,26 @@ namespace rtti {
         template <typename ATTRIBUTE> const ATTRIBUTE* attribute() const;
 
         virtual Object new_object() const = 0;
-        virtual Object call_constructor(BufferRef&& buff) const = 0;
+        virtual Object new_copy(const ObjectRef& src) const = 0;
+        virtual Object new_move(ObjectRef& src) const = 0;
         virtual void delete_object(Object&& obj) const = 0;
-        virtual BufferRef call_destructor(Object&& obj) const = 0;
+
+        bool is_copyable() const;
+        bool is_movable() const;
+
+        virtual Res<void, ErrConstruct> can_construct(const BufferRef& buff) const = 0;
+        virtual ObjectRef construct(BufferRef&& buff) const = 0;
+        virtual Object construct(Buffer&& buff) const = 0;
+        virtual bool can_copy_construct(const BufferRef& buff, const ObjectRef& src) const = 0;
+        virtual ObjectRef copy_construct(BufferRef&& buff, const ObjectRef& src) const = 0;
+        virtual Object copy_construct(Buffer&& buff, const ObjectRef& src) const = 0;
+        virtual ObjectRef move_construct(BufferRef&& buff, ObjectRef& src) const = 0;
+        virtual Object move_construct(Buffer&& buff, ObjectRef& src) const = 0;
+        virtual BufferRef destruct(ObjectRef&& obj) const = 0;
+        virtual Buffer destruct(Object&& obj) const = 0;
+        
         virtual void copy(ObjectRef& dst, const ObjectRef& src) const = 0;
-        virtual Object copy_construct(const ObjectRef& src) const = 0;
         virtual void move(ObjectRef& dst, ObjectRef& src) const = 0;
-        virtual Object move_construct(ObjectRef& src) const = 0;
 
         virtual const Fundamental* as_fundamental() const = 0;
         virtual const Enum* as_enum() const = 0;
