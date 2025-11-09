7284597641dfc0ff1a0fd4132e896e2c47959fa2
Piotr Squares Mościcki <m.piotr@moscicki.pl>
2025-11-09 12:51:05 +0000

    Add Class::copy_assign tests

--- test/src/class.cpp
+++ test/src/class.cpp
@@ -63,7 +63,9 @@ public:
     TestClassNotMoveAssignable(const TestClassNotMoveAssignable& other) = default;
     TestClassNotMoveAssignable(TestClassNotMoveAssignable&& other) = default;
     TestClassNotMoveAssignable& operator=(const TestClassNotMoveAssignable& other) = default;
-    TestClassNotMoveAssignable& operator=(TestClassNotMoveAssignable&& other) = delete;
+    TestClassNotMoveAssignable& operator=(TestClassNotMoveAssignable&& other) = delete; // DELETE
+    
+    unsigned int m_int_val = 0;
 };
 
 class TestClassNotCopyAssignable {
@@ -74,7 +76,7 @@ public:
     TestClassNotCopyAssignable() = default;
     TestClassNotCopyAssignable(const TestClassNotCopyAssignable& other) = default;
     TestClassNotCopyAssignable(TestClassNotCopyAssignable&& other) = default;
-    TestClassNotCopyAssignable& operator=(const TestClassNotCopyAssignable& other) = delete;
+    TestClassNotCopyAssignable& operator=(const TestClassNotCopyAssignable& other) = delete; // DELETE
     TestClassNotCopyAssignable& operator=(TestClassNotCopyAssignable&& other) {
         m_int_val = other.m_int_val;
         other.m_int_val = 0xDEADBEEF;
@@ -110,6 +112,45 @@ TEST_CASE( "rtti::Class::== from differend dll", "[rtti::Class]" ) {
 TEST_CASE( "rtti::Class::name", "[rtti::Class]" ) {
     REQUIRE( static_type<TestClass1>()->name() == "test::TestClass1" );
     REQUIRE( static_type<TestClass2>()->name() == "TestClass2" );
+}
+
+//*************************************************************************************************
+TEST_CASE( "rtti::Class::copy_assign", "[rtti::Class]" ) {
+    Object src;
+    Object dst;
+
+    REQUIRE( 
+        static_type<TestClassNotCopyAssignable>()->copy_assign(dst, src).err() 
+        == 
+        Type::ErrCopy::NOT_COPY_ASSIGNABLE);
+        
+    REQUIRE( 
+        static_type<TestClassNotMoveAssignable>()->copy_assign(dst, src).err() 
+        == 
+        Type::ErrCopy::INVALID_DESTINATION_OBJECT);
+
+    dst = static_type<TestClassNotCopyAssignable>()->new_default().ok();
+    REQUIRE( 
+        static_type<TestClassNotMoveAssignable>()->copy_assign(dst, src).err() 
+        == 
+        Type::ErrCopy::INCORRECT_DESTINATION_OBJECT_TYPE );
+
+    dst = static_type<TestClassNotMoveAssignable>()->new_default().ok();
+    REQUIRE( 
+        static_type<TestClassNotMoveAssignable>()->copy_assign(dst, src).err() 
+        == 
+        Type::ErrCopy::INVALID_SOURCE_OBJECT );
+
+    src = static_type<TestClassNotCopyAssignable>()->new_default().ok();
+    REQUIRE(
+        static_type<TestClassNotMoveAssignable>()->copy_assign(dst, src).err() 
+        == 
+        Type::ErrCopy::INCORRECT_SOURCE_OBJECT_TYPE );
+
+    src = static_type<TestClassNotMoveAssignable>()->new_default().ok();
+    src.value_as<TestClassNotMoveAssignable>().ok()->m_int_val = 6565656;
+    REQUIRE( static_type<TestClassNotMoveAssignable>()->copy_assign(dst, src).is_ok() == true );
+    REQUIRE( dst.value_as<TestClassNotMoveAssignable>().ok()->m_int_val == 6565656 );
 }
 
 //*************************************************************************************************
